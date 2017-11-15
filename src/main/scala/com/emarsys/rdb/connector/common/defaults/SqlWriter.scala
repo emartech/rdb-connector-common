095074fe7d79443082016bc49f6c95c7f3e75423
khorvath <kristof.horvath@emarsys.com>
2017-11-15 16:42:28 +0100

    [SimpleQuery] added SimpleQuery to sql string converter

---
+++ src/main/scala/com/emarsys/rdb/connector/common/defaults/SqlWriter.scala
@@ -0,0 +1,115 @@
+package com.emarsys.rdb.connector.common.defaults
+
+import scala.language.implicitConversions
+import com.emarsys.rdb.connector.common.models.SimpleSelect
+import com.emarsys.rdb.connector.common.models.SimpleSelect._
+
+import scala.annotation.implicitNotFound
+
+@implicitNotFound(msg = "Cannot find implicit SqlWriter type class for ${T}")
+trait SqlWriter[T] {
+  def write(obj: T): String
+}
+
+object SqlWriter {
+
+  class WritableSqlElement[T](any: T) {
+    def toSql(implicit writer: SqlWriter[T]): String = writer.write(any)
+  }
+
+  implicit def createWritableSqlElement[T](any: T): WritableSqlElement[T] = new WritableSqlElement(any)
+
+  implicit def func2LazyWriter[T](f: T => String): SqlWriter[T] = lazyWriter((obj: T) => f(obj))
+
+  def lazyWriter[T](writer: => SqlWriter[T]): SqlWriter[T] = new SqlWriter[T] {
+    private lazy val delegate = writer
+
+    def write(x: T): String = delegate.write(x)
+  }
+
+  def createTableNameWriter(symbol: String, escape: String): SqlWriter[TableName] =
+    (tableName: TableName) => createEscapeQuoter(symbol, escape, tableName.t)
+
+  def createFieldNameWriter(symbol: String, escape: String): SqlWriter[FieldName] =
+    (fieldName: FieldName) => createEscapeQuoter(symbol, escape, fieldName.f)
+
+  def createValueWriter(symbol: String, escape: String): SqlWriter[Value] =
+    (value: Value) => createEscapeQuoter(symbol, escape, value.v)
+
+  def createAllFieldWriter(symbol: String): SqlWriter[AllField.type] =
+    (_: AllField.type) => symbol
+
+  def createSpecificFieldsWriter(symbol: String)(implicit w: SqlWriter[FieldName]): SqlWriter[SpecificFields] =
+    (x: SpecificFields) => x.fields.map(_.toSql).mkString(symbol)
+
+  def createIsNullWriter(isNullFormat: String)(implicit w: SqlWriter[FieldName]): SqlWriter[IsNull] =
+    (x: IsNull) => isNullFormat.format(x.field.toSql)
+
+  def createNotNullWriter(notNullFormat: String)(implicit w: SqlWriter[FieldName]): SqlWriter[NotNull] =
+    (x: NotNull) => notNullFormat.format(x.field.toSql)
+
+  def createEqualToValueWriter(symbol: String)(implicit w1: SqlWriter[FieldName], w2: SqlWriter[Value]): SqlWriter[EqualToValue] =
+    (x: EqualToValue) => s"${x.field.toSql}$symbol${x.value.toSql}"
+
+  def createEscapeQuoter(symbol: String, escape: String, name: String): String = {
+    s"$symbol${escaping(symbol,escape,name)}$symbol"
+  }
+
+  def conditionWriter(symbol: String, conditions: Seq[WhereCondition])(implicit w: SqlWriter[WhereCondition]): String = {
+    if (conditions.size == 1) {
+      conditions.head.toSql
+    } else {
+      s"(${conditions.map(_.toSql).mkString(symbol)})"
+    }
+  }
+
+  private def escaping(symbol: String, escape: String, text: String): String = {
+    if (text == null) {
+      null
+    } else {
+      val escapedText = if(escape == "") text else text.replace(escape, escape * 2)
+      val symbolEscapedText = if(escape == "" || symbol == "") escapedText else escapedText.replace(symbol, s"$escape$symbol")
+      symbolEscapedText
+    }
+  }
+
+}
+
+trait DefaultSqlWriters {
+
+  import SqlWriter._
+
+  implicit lazy val tableNameWriter: SqlWriter[TableName] = createTableNameWriter("\"", "\\")
+  implicit lazy val fieldNameWriter: SqlWriter[FieldName] = createFieldNameWriter("\"", "\\")
+  implicit lazy val valueWriter: SqlWriter[Value] = createValueWriter("'", "\\")
+  implicit lazy val allFieldWriter: SqlWriter[AllField.type] = createAllFieldWriter("*")
+  implicit lazy val specificFieldsWriter: SqlWriter[SpecificFields] = createSpecificFieldsWriter(",")
+  implicit lazy val isNullWriter: SqlWriter[IsNull] = createIsNullWriter("%s IS NULL")
+  implicit lazy val notNullWriter: SqlWriter[NotNull] = createNotNullWriter("%s IS NOT NULL")
+  implicit lazy val equalToValueWriter: SqlWriter[EqualToValue] = createEqualToValueWriter("=")
+  implicit lazy val orWriter: SqlWriter[Or] = (x: Or) => conditionWriter(" OR ", x.conditions)
+  implicit lazy val andWriter: SqlWriter[And] = (x: And) => conditionWriter(" AND ", x.conditions)
+
+  implicit lazy val whereConditionWriter: SqlWriter[WhereCondition] = {
+    case x: EqualToValue => x.toSql
+    case x: IsNull => x.toSql
+    case x: NotNull => x.toSql
+    case x: Or => x.toSql
+    case x: And => x.toSql
+  }
+
+  implicit lazy val fieldsWriter: SqlWriter[Fields] = {
+    case x: AllField.type => x.toSql
+    case x: SpecificFields => x.toSql
+  }
+
+  implicit lazy val simpleSelectWriter: SqlWriter[SimpleSelect] = (ss: SimpleSelect) => {
+    val head = s"SELECT ${ss.fields.toSql} FROM ${ss.table.toSql}"
+    val where = ss.where.map(_.toSql).map(" WHERE " + _).getOrElse("")
+    val limit = ss.limit.map(" LIMIT " + _).getOrElse("")
+
+    s"$head$where$limit"
+  }
+}
+
+object DefaultSqlWriters extends DefaultSqlWriters
---
+++ src/test/scala/com/emarsys/rdb/connector/common/defaults/DefaultSqlWriterSpec.scala
@@ -0,0 +1,371 @@
+package com.emarsys.rdb.connector.common.defaults
+
+import com.emarsys.rdb.connector.common.models.SimpleSelect._
+import org.scalatest.{Matchers, WordSpecLike}
+import SqlWriter._
+import com.emarsys.rdb.connector.common.models.SimpleSelect
+
+class DefaultSqlWriterSpec extends WordSpecLike with Matchers {
+
+  "DefaultSqlWriter" when {
+
+    "TableName" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        TableName("TABLE1").toSql shouldEqual """"TABLE1""""
+      }
+
+      "use default writer - escape" in {
+        import DefaultSqlWriters._
+
+        TableName("""TA\BLE"1""").toSql shouldEqual """"TA\\BLE\"1""""
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val tableNameWriter: SqlWriter[TableName] = SqlWriter.createTableNameWriter("#", "\\")
+        }
+        import customWriters._
+
+        TableName("TABLE1").toSql shouldEqual "#TABLE1#"
+      }
+    }
+
+    "FieldName" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        FieldName("FIELD1").toSql shouldEqual """"FIELD1""""
+      }
+
+      "use default writer - escape" in {
+        import DefaultSqlWriters._
+
+        FieldName("""F"IEL\D1""").toSql shouldEqual """"F\"IEL\\D1""""
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val fieldNameWriter: SqlWriter[FieldName] = SqlWriter.createFieldNameWriter("#", "\\")
+        }
+        import customWriters._
+
+        FieldName("FIELD1").toSql shouldEqual "#FIELD1#"
+      }
+    }
+
+    "Value" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        Value("VALUE1").toSql shouldEqual "'VALUE1'"
+      }
+
+      "use default writer - escape" in {
+        import DefaultSqlWriters._
+
+        Value("""VA'L'UE\1""").toSql shouldEqual """'VA\'L\'UE\\1'"""
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val valueWriter: SqlWriter[Value] = SqlWriter.createValueWriter("#", "\\")
+        }
+        import customWriters._
+
+        Value("VALUE1").toSql shouldEqual "#VALUE1#"
+      }
+    }
+
+    "AllFields" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        AllField.toSql shouldEqual "*"
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val allFieldWriter: SqlWriter[SimpleSelect.AllField.type] = SqlWriter.createAllFieldWriter("#")
+        }
+        import customWriters._
+
+        AllField.toSql shouldEqual "#"
+      }
+    }
+
+    "SpecificFields" should {
+
+      "use default writer - one field" in {
+        import DefaultSqlWriters._
+
+        SpecificFields(Seq(FieldName("FIELD1"))).toSql shouldEqual """"FIELD1""""
+      }
+
+      "use default writer - many fields" in {
+        import DefaultSqlWriters._
+
+        SpecificFields(Seq(FieldName("FIELD1"), FieldName("FIELD2"), FieldName("FIELD3"))).toSql shouldEqual """"FIELD1","FIELD2","FIELD3""""
+      }
+
+      "use custom writer - many fields" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val specificFieldsWriter: SqlWriter[SpecificFields] = SqlWriter.createSpecificFieldsWriter("#")
+        }
+        import customWriters._
+
+        SpecificFields(Seq(FieldName("FIELD1"), FieldName("FIELD2"), FieldName("FIELD3"))).toSql shouldEqual """"FIELD1"#"FIELD2"#"FIELD3""""
+      }
+    }
+
+    "IsNull" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        IsNull(FieldName("FIELD1")).toSql shouldEqual """"FIELD1" IS NULL"""
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val isNullWriter: SqlWriter[IsNull] = SqlWriter.createIsNullWriter("<<IS %s NULL>>")
+        }
+        import customWriters._
+
+        IsNull(FieldName("FIELD1")).toSql shouldEqual """<<IS "FIELD1" NULL>>"""
+      }
+    }
+
+    "IsNotNull" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        NotNull(FieldName("FIELD1")).toSql shouldEqual """"FIELD1" IS NOT NULL"""
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val notNullWriter: SqlWriter[NotNull] = SqlWriter.createNotNullWriter("<<IS NOT %s NULL>>")
+        }
+        import customWriters._
+
+        NotNull(FieldName("FIELD1")).toSql shouldEqual """<<IS NOT "FIELD1" NULL>>"""
+      }
+    }
+
+    "EqualToValue" should {
+
+      "use default writer" in {
+        import DefaultSqlWriters._
+
+        EqualToValue(FieldName("FIELD1"), Value("VALUE1")).toSql shouldEqual """"FIELD1"='VALUE1'"""
+      }
+
+      "use custom writer" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val equalToValueWriter: SqlWriter[EqualToValue] = SqlWriter.createEqualToValueWriter(" IS ")
+        }
+        import customWriters._
+
+        EqualToValue(FieldName("FIELD1"), Value("VALUE1")).toSql shouldEqual """"FIELD1" IS 'VALUE1'"""
+      }
+    }
+
+    "Or" should {
+
+      "use default writer - one condition" in {
+        import DefaultSqlWriters._
+
+        Or(Seq(EqualToValue(FieldName("FIELD1"), Value("VALUE1")))).toSql shouldEqual """"FIELD1"='VALUE1'"""
+      }
+
+      "use default writer - many condition" in {
+        import DefaultSqlWriters._
+
+        Or(Seq(IsNull(FieldName("FIELD1")), EqualToValue(FieldName("FIELD2"), Value("VALUE2")))).toSql shouldEqual """("FIELD1" IS NULL OR "FIELD2"='VALUE2')"""
+      }
+
+
+      "use default writer - inner or" in {
+        import DefaultSqlWriters._
+
+        Or(Seq(IsNull(FieldName("FIELD1")), Or(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3")))))).toSql shouldEqual """("FIELD1" IS NULL OR ("FIELD2" IS NULL OR "FIELD3"='VALUE3'))"""
+      }
+
+      "use custom writer - inner or" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val orWriter: SqlWriter[Or] = (x: Or) => conditionWriter(" || ", x.conditions)
+        }
+        import customWriters._
+
+        Or(Seq(IsNull(FieldName("FIELD1")), Or(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3")))))).toSql shouldEqual """("FIELD1" IS NULL || ("FIELD2" IS NULL || "FIELD3"='VALUE3'))"""
+      }
+    }
+
+    "And" should {
+
+      "use default writer - one condition" in {
+        import DefaultSqlWriters._
+
+        And(Seq(EqualToValue(FieldName("FIELD1"), Value("VALUE1")))).toSql shouldEqual """"FIELD1"='VALUE1'"""
+      }
+
+      "use default writer - many condition" in {
+        import DefaultSqlWriters._
+
+        And(Seq(IsNull(FieldName("FIELD1")), EqualToValue(FieldName("FIELD2"), Value("VALUE2")))).toSql shouldEqual """("FIELD1" IS NULL AND "FIELD2"='VALUE2')"""
+      }
+
+
+      "use default writer - inner or" in {
+        import DefaultSqlWriters._
+
+        And(Seq(IsNull(FieldName("FIELD1")), And(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3")))))).toSql shouldEqual """("FIELD1" IS NULL AND ("FIELD2" IS NULL AND "FIELD3"='VALUE3'))"""
+      }
+
+      "use custom writer - inner or" in {
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val andWriter: SqlWriter[And] = (x: And) => conditionWriter(" && ", x.conditions)
+        }
+        import customWriters._
+
+        And(Seq(IsNull(FieldName("FIELD1")), And(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3")))))).toSql shouldEqual """("FIELD1" IS NULL && ("FIELD2" IS NULL && "FIELD3"='VALUE3'))"""
+      }
+    }
+
+    "WhereCondition" should {
+
+      "combinate all element - use default writers" in {
+        import DefaultSqlWriters._
+
+        val where =
+          And(Seq(
+            IsNull(FieldName("FIELD1")),
+            Or(Seq(
+              IsNull(FieldName("FIELD2")),
+              EqualToValue(FieldName("FIELD3"), Value("VALUE3")),
+              NotNull(FieldName("FIELD4"))
+            )),
+            Or(Seq(
+              IsNull(FieldName("FIELD5"))
+            ))
+          ))
+
+
+        where.toSql shouldEqual """("FIELD1" IS NULL AND ("FIELD2" IS NULL OR "FIELD3"='VALUE3' OR "FIELD4" IS NOT NULL) AND "FIELD5" IS NULL)"""
+      }
+
+      "combinate all element - use custom writers" in {
+
+        val customWriters = new DefaultSqlWriters {
+          override implicit lazy val fieldNameWriter: SqlWriter[FieldName] = SqlWriter.createFieldNameWriter("#", "\\")
+          override implicit lazy val valueWriter: SqlWriter[Value] = SqlWriter.createValueWriter("`", "\\")
+          override implicit lazy val notNullWriter: SqlWriter[NotNull] = SqlWriter.createNotNullWriter("<<IS NOT %s NULL>>")
+          override implicit lazy val equalToValueWriter: SqlWriter[EqualToValue] = SqlWriter.createEqualToValueWriter(" == ")
+          override implicit lazy val isNullWriter: SqlWriter[IsNull] = SqlWriter.createIsNullWriter("NULL(%s)")
+          override implicit lazy val orWriter: SqlWriter[Or] = (x: Or) => conditionWriter(" || ", x.conditions)
+          override implicit lazy val andWriter: SqlWriter[And] = (x: And) => conditionWriter(" && ", x.conditions)
+        }
+        import customWriters._
+
+        val where =
+          And(Seq(
+            IsNull(FieldName("FIELD1")),
+            Or(Seq(
+              IsNull(FieldName("FIELD2")),
+              EqualToValue(FieldName("FIELD3"), Value("VALUE3")),
+              NotNull(FieldName("FIELD4"))
+            )),
+            Or(Seq(
+              IsNull(FieldName("FIELD5"))
+            ))
+          ))
+
+
+        where.toSql shouldEqual """(NULL(#FIELD1#) && (NULL(#FIELD2#) || #FIELD3# == `VALUE3` || <<IS NOT #FIELD4# NULL>>) && NULL(#FIELD5#))"""
+      }
+    }
+
+    "SimpleSelect" should {
+
+      "use default writer - minimal" in {
+        import DefaultSqlWriters._
+
+        val select = SimpleSelect(
+          fields = AllField,
+          table = TableName("TABLE1")
+        )
+
+        select.toSql shouldEqual """SELECT * FROM "TABLE1""""
+      }
+
+      "use default writer - specific fields" in {
+        import DefaultSqlWriters._
+
+        val select = SimpleSelect(
+          fields = SpecificFields(Seq(FieldName("FIELD1"), FieldName("FIELD2"), FieldName("FIELD3"))),
+          table = TableName("TABLE1")
+        )
+
+        select.toSql shouldEqual """SELECT "FIELD1","FIELD2","FIELD3" FROM "TABLE1""""
+      }
+
+      "use default writer - where" in {
+        import DefaultSqlWriters._
+
+        val select = SimpleSelect(
+          fields = AllField,
+          table = TableName("TABLE1"),
+          where = Some(And(Seq(IsNull(FieldName("FIELD1")), And(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3")))))))
+        )
+
+        select.toSql shouldEqual """SELECT * FROM "TABLE1" WHERE ("FIELD1" IS NULL AND ("FIELD2" IS NULL AND "FIELD3"='VALUE3'))"""
+      }
+
+      "use default writer - limit" in {
+        import DefaultSqlWriters._
+
+        val select = SimpleSelect(
+          fields = AllField,
+          table = TableName("TABLE1"),
+          limit = Some(100)
+        )
+
+        select.toSql shouldEqual """SELECT * FROM "TABLE1" LIMIT 100"""
+      }
+
+      "use default writer - where and limit" in {
+        import DefaultSqlWriters._
+
+        val select = SimpleSelect(
+          fields = AllField,
+          table = TableName("TABLE1"),
+          where = Some(And(Seq(IsNull(FieldName("FIELD1")), And(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3"))))))),
+          limit = Some(100)
+        )
+
+        select.toSql shouldEqual """SELECT * FROM "TABLE1" WHERE ("FIELD1" IS NULL AND ("FIELD2" IS NULL AND "FIELD3"='VALUE3')) LIMIT 100"""
+      }
+
+      "use default writer - full" in {
+        import DefaultSqlWriters._
+
+        val select = SimpleSelect(
+          fields = SpecificFields(Seq(FieldName("FIELD1"), FieldName("FIELD2"), FieldName("FIELD3"))),
+          table = TableName("TABLE1"),
+          where = Some(And(Seq(IsNull(FieldName("FIELD1")), And(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3"))))))),
+          limit = Some(100)
+        )
+
+        select.toSql shouldEqual """SELECT "FIELD1","FIELD2","FIELD3" FROM "TABLE1" WHERE ("FIELD1" IS NULL AND ("FIELD2" IS NULL AND "FIELD3"='VALUE3')) LIMIT 100"""
+      }
+    }
+
+  }
+}
